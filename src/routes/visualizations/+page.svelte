eb6696dea16aa9d474f4cab01334a7514670ec7b
Destructor_Ben <ben.honeyfield@gmail.com>
2025-07-05 19:33:27 +1200

    Work on a better visualization list

--- src/routes/visualizations/+page.svelte
+++ src/routes/visualizations/+page.svelte
@@ -1,1 +1,53 @@
-<a class="link" href="/visualizations/taylor-series">Taylor Series</a>
+<script>
+	import Graph from "$lib/components/Graph.svelte";
+	import VisualizationLink from "./VisualizationLink.svelte";
+
+</script>
+<div class="visualization-list">
+  TODO: we need an OnResize callback and OnHover callback
+  <VisualizationLink title="Taylor Series" link="/visualizations/taylor-series">
+    TODO: make the preview look like an animated taylor series on hover
+    <Graph
+      id="taylor-series"
+      width={800}
+      height={400}
+    />
+  </VisualizationLink>
+
+  <VisualizationLink title="Newton's Method" link="/visualizations/newtons-method">
+    TODO: make the preview look like an animated newtons method
+  </VisualizationLink>
+
+  <VisualizationLink title="Mean Value & Rolle's Theorem" link="/visualizations/mvt-and-rolles-theorem">
+    TODO: make the preview look like an animated MVT visual
+  </VisualizationLink>
+
+  <!-- Below are temporary and are for testing -->
+  <VisualizationLink title="Taylor Series" link="/visualizations/taylor-series">
+    Test
+  </VisualizationLink>
+
+  <VisualizationLink title="Taylor Series" link="/visualizations/taylor-series">
+    Test
+  </VisualizationLink>
+
+  <VisualizationLink title="Taylor Series" link="/visualizations/taylor-series">
+    Test
+  </VisualizationLink>
+
+  <VisualizationLink title="Taylor Series" link="/visualizations/taylor-series">
+    Test
+  </VisualizationLink>
+
+  <VisualizationLink title="Taylor Series" link="/visualizations/taylor-series">
+    Test
+  </VisualizationLink>
+</div>
+
+<style>
+  .visualization-list {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+    gap: 1em;
+  }
+</style>
---
+++ src/routes/visualizations/VisualizationLink.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface Props {
+    title: string,
+    link: string,
+    children: Snippet // Graph, image, etc. drawn as an interactive display
+  }
+
+  let {
+    title,
+    link,
+    children
+  }: Props = $props();
+</script>
+
+<a class="mg visualization-button" href={link}>
+  <div class=thumbnail>
+    {@render children()}
+  </div>
+
+  <h3>{title}</h3>
+</a>
+
+<style>
+  .visualization-button {
+    gap: 1em;
+    height: 300px;
+    text-decoration: none;
+    border: var(--border);
+
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+
+    .thumbnail {
+      border-radius: 1em;
+      height: 100%;
+      overflow: hidden;
+    }
+    
+    border-color: transparent;
+    transition-property: border-color, background-color;
+
+    &:hover {
+      border-color: var(--col-highlight);
+      background-color: var(--col-fg);
+    }
+
+    &:active {
+      background-color: var(--col-highlight);
+    }
+  }
+</style>
