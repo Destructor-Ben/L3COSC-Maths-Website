2d4e093fd3b316eee1b2b94c06bc133beb2e19ba
Destructor_Ben <ben.honeyfield@gmail.com>
2025-07-18 09:12:05 +1200

    Finish legal documents

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,16 @@
-<h1>About</h1>
+<div class="mg">
+  <h1>About</h1>
+</div>
+
+<div class="mg">
+  
+</div>
+
+<style>
+  :global {
+    main {
+      gap: 1em;
+    }
+  }
+</style>
+
--- src/routes/about/privacy-policy/+page.svelte
+++ src/routes/about/privacy-policy/+page.svelte
@@ -3,19 +3,17 @@
 </div>
 
 <div class="mg">
-  <p><strong>Last Updated:</strong> July 11, 2025</p>
-
   <h2>Introduction</h2>
-  <p>This Privacy Policy explains how [Your Website Name] ("we," "us," or "our") handles user information on our website [Your Website URL] (the "Website"). We are committed to protecting your privacy. This policy is designed to inform you that our Website does not collect, store, or process any personal data from its users.</p>
+  <p>This Privacy Policy explains how Phobos ("we," "us," or "our") handles user information on our website (the "Website"). We are committed to protecting your privacy. This policy is designed to inform you that our Website does not collect, store, or process any personal data from its users.</p>
 
   <br />
   <h2>No Collection of Personal Data</h2>
   <p>We do not collect, store, or process any personal information or user data through the Website. This includes, but is not limited to:</p>
   <ul>
-      <li>Names, email addresses, or other contact information</li>
-      <li>IP addresses, browser details, or device information</li>
-      <li>Cookies or tracking technologies</li>
-      <li>Any other identifiable information</li>
+    <li>Names, email addresses, or other contact information</li>
+    <li>IP addresses, browser details, or device information</li>
+    <li>Cookies or tracking technologies</li>
+    <li>Any other identifiable information</li>
   </ul>
   <p>As such, no personal data is gathered when you visit or interact with our Website.</p>
 
@@ -27,9 +25,8 @@
   <h2>Cookies</h2>
   <p>We do not use cookies or similar tracking technologies on our Website.</p>
 
-  <br />
-  <h2>Changes to This Privacy Policy</h2>
-  <p>We may update this Privacy Policy from time to time to reflect changes in our practices or legal requirements. Any updates will be posted on this page with an updated "Last Updated" date. We encourage you to review this policy periodically.</p>
+  <hr>
+  <p>Thank you for using Phobos. We hope you have a positive experience.</p>
 </div>
 
 <style>
