5667bfe97405d650c2bfd226c148e1a4b2d093ce
Destructor_Ben <62684035+Destructor-Ben@users.noreply.github.com>
2025-08-11 11:59:21 +1200

    Fix one more :global

--- src/routes/visualizations/taylor-series/+page.svelte
+++ src/routes/visualizations/taylor-series/+page.svelte
@@ -99,92 +99,95 @@
   <title>Phobos - Taylor Series</title>
 </svelte:head>
 
-<div class="mg">
-  <h1>Taylor Series</h1>
-</div>
 
-<div class="mg taylor-series">
-  <h2>Explanation</h2>
+<div class="container">
+  <div class="mg">
+    <h1>Taylor Series</h1>
+  </div>
 
-  <p class="description">
-    A taylor series is an infinite sum of polynomials used to approximate the original function, but only near the point a, the point the series is calculated around.
-    <br />
-    <br />
-    A taylor series works by calculating the derivative of the original function at the point a, and integrating these derivatives to create a polynomial that will eventually converge to the original function.
-    <br />
-    <br />
-    Not all taylor series will converge, and some will only converge for a limited range of x values.
-    <br />
-    <br />
-    The equation for a taylor series is given by the following formula:
-    <br />
-    <br />
-    <Math latex={latex.taylorSeries} displayMode />
-  </p>
+  <div class="mg taylor-series">
+    <h2>Explanation</h2>
 
-  <hr />
+    <p class="description">
+      A taylor series is an infinite sum of polynomials used to approximate the original function, but only near the point a, the point the series is calculated around.
+      <br />
+      <br />
+      A taylor series works by calculating the derivative of the original function at the point a, and integrating these derivatives to create a polynomial that will eventually converge to the original function.
+      <br />
+      <br />
+      Not all taylor series will converge, and some will only converge for a limited range of x values.
+      <br />
+      <br />
+      The equation for a taylor series is given by the following formula:
+      <br />
+      <br />
+      <Math latex={latex.taylorSeries} displayMode />
+    </p>
 
-  <h2>Visualization</h2>
+    <hr />
 
-  <div class="inputs">
-    <div>
-      <span>Function</span>
-      <select name="test" id="test" bind:value={currentFunctionName}>
-        <option value="sin">sin(x)</option>
-        <option value="cos">cos(x)</option>
-        <option value="tan">tan(x)</option>
-        <option value="exp">e^x</option>
-      </select>
-    </div>
+    <h2>Visualization</h2>
 
-    <div>
-      <button class="button" onclick={() => setIterations(iterations + 1)}>Next Iteration</button>
-      <button class="button" onclick={() => setIterations(iterations - 1)}>Previous Iteration</button>
+    <div class="inputs">
+      <div>
+        <span>Function</span>
+        <select name="test" id="test" bind:value={currentFunctionName}>
+          <option value="sin">sin(x)</option>
+          <option value="cos">cos(x)</option>
+          <option value="tan">tan(x)</option>
+          <option value="exp">e^x</option>
+        </select>
+      </div>
+
+      <div>
+        <button class="button" onclick={() => setIterations(iterations + 1)}>Next Iteration</button>
+        <button class="button" onclick={() => setIterations(iterations - 1)}>Previous Iteration</button>
+      </div>
+      
+      <div>
+        <span><Math latex="iterations = {iterations}" /> (between 0 and {maxIterations})</span>
+        <input
+          type="range"
+          bind:value={
+            () => iterations,
+            (value) => setIterations(value, false)
+          }
+          min={0}
+          max={15}
+        />
+      </div>
+
+      <div>
+        <span><Math latex="a = {a}" /> (between -10 and 10)</span>
+        <input type="range" name="test2" id="test2" bind:value={a} min={-10} max={10} step={0.01} />
+      </div>
     </div>
     
-    <div>
-      <span><Math latex="iterations = {iterations}" /> (between 0 and {maxIterations})</span>
-      <input
-        type="range"
-        bind:value={
-          () => iterations,
-          (value) => setIterations(value, false)
+    <Graph
+      id="taylor-series-graph"
+      width={700}
+      height={500}
+      initialScale={{ x: 1 / 10, y: 1 / 10 }}
+      hasNiceBorders={true}
+      functions={[
+        {
+          name: "f(x)",
+          color: () => Colors.ContrastRed,
+          func: currentFunction,
+          getDomains: getFunctionDomains(),
+        },
+        {
+          name: "taylor(x)",
+          color: () => Colors.ContrastBlue,
+          func: (x: number) => lerp(
+            taylor(x, a, previousIterations, getFunctionDerivative()),
+            taylor(x, a, iterations, getFunctionDerivative()),
+            smoothstep(animationProgress)
+          )
         }
-        min={0}
-        max={15}
-      />
-    </div>
-
-    <div>
-      <span><Math latex="a = {a}" /> (between -10 and 10)</span>
-      <input type="range" name="test2" id="test2" bind:value={a} min={-10} max={10} step={0.01} />
-    </div>
+      ]}
+    />
   </div>
-  
-  <Graph
-    id="taylor-series-graph"
-    width={700}
-    height={500}
-    initialScale={{ x: 1 / 10, y: 1 / 10 }}
-    hasNiceBorders={true}
-    functions={[
-      {
-        name: "f(x)",
-        color: () => Colors.ContrastRed,
-        func: currentFunction,
-        getDomains: getFunctionDomains(),
-      },
-      {
-        name: "taylor(x)",
-        color: () => Colors.ContrastBlue,
-        func: (x: number) => lerp(
-          taylor(x, a, previousIterations, getFunctionDerivative()),
-          taylor(x, a, iterations, getFunctionDerivative()),
-          smoothstep(animationProgress)
-        )
-      }
-    ]}
-  />
 </div>
 
 <style>
@@ -210,12 +213,16 @@
       gap: 1em;
     }
   }
+  
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: center;
+    gap: 1em;
+  }
 
   :global {
-    main {
-      gap: 1em;
-    }
-
     .description math {
       font-size: 2.5rem;
     }
