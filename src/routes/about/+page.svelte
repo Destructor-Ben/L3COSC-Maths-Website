907959e1e827078017d19cc6368b5f014b076466
Destructor_Ben <62684035+Destructor-Ben@users.noreply.github.com>
2025-08-11 12:23:49 +1200

    New about page styling

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,13 +2,17 @@
   <title>Phobos - About</title>
 </svelte:head>
 
+<!--
+  A container is needed since we want the content to have a gap, but we can't set it on <main>
+  without messing with other pages.
+ -->
 <div class="container">
-  <div class="mg">
+  <div class="mg heading">
     <h1>About</h1>
   </div>
 
-  <div class="mg about-container">
-    <div class="about">
+  <div class="mg about">
+    <div>
       <p>
         Phobos is an online maths visualization tool created by Destructor_Ben,
         intended for use by Maths students in New Zealand studying NCEA or at university.
@@ -35,27 +39,41 @@
         Phobos was also initially created as a project for the Level 3 Computer Science course at my school,
         though I have plans to continue developing it and make it more than just a school project.
       </p>
+
+      <hr />
     </div>
   </div>
 </div>
 
 <style>
   .container {
+    /* Grow to fill the parent container */
+    flex: 1;
+
+    /* Set flex styles, including gap (which can't be set on <main> without messing with other pages) */
     display: flex;
     flex-direction: column;
-    align-items: stretch;
+    align-items: center;
     justify-content: center;
     gap: 1em;
   }
 
-  .about-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  .heading {
+    /* Text being too wide makes it hard to read */
+    width: var(--article-width);
   }
 
   .about {
-    max-width: 800px;
+    /* Text being too wide makes it hard to read */
+    width: var(--article-width);
+    
+    /* Text requires a larger amount of padding to look nice */
+    padding: var(--article-padding);
+
+    /* Fixes the horizontal rule adding making the div seem too large */
+    hr {
+      margin-bottom: 0;
+    }
   }
 </style>
 
