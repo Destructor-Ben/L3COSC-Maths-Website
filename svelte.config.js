ee17875001d60a9ee3a58e38f959adc188185eeb
Destructor_Ben <62684035+Destructor-Ben@users.noreply.github.com>
2025-08-15 09:48:30 +1200

    Revert "Update svelte.config.js"
    This reverts commit 790011455738e48e93a802b129078b0db232c3e6.

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const basePath = process.argv.includes('dev') ? '' : process.env.BASE_PATH;
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   // Consult https://svelte.dev/docs/kit/integrations
@@ -14,13 +12,13 @@ export default {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      fallback: basePath + "/404.html", // Needed for GitHub Pages
+      fallback: "404.html", // Needed for GitHub Pages
     }),
 
     // Set the prefix for all pages in the url (such as '/phobos/' for GitHub Pages so it can be hosted there)
     // Don't set it in the development environment since we don't need it there
     paths: {
-      base: basePath
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
     },
   },
 };
