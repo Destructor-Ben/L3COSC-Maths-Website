9b8370e08249abe67ca979d4053dd17f9ceb5b7f
Destructor_Ben <62684035+Destructor-Ben@users.noreply.github.com>
2025-08-18 13:24:47 +1200

    More work on the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,96 +1,144 @@
 <script lang="ts">
   import Graph from "$lib/components/Graph.svelte";
-  import { Math } from "svelte-math";
+  import type { Domain } from "$lib/maths/function";
   import * as Styles from "$lib/styles";
   import { Sin } from "$lib/maths/functions";
-  import type { Domain } from "$lib/maths/function";
   import { lerp } from "$lib/maths/lerp";
-
-  let width = $state(0);
-  let height = $state(0);
-
-  function handleWindowResize()
-  {
-    console.log("handleWindowResize")
-    width = window.innerWidth;
-    height = window.innerHeight;
-  }
-
-  // Set initial dimensions
-  //$effect(() => {
-    //handleWindowResize();
-  //});
-
-  // Calculate scroll progress
-  //let scrollProgress = $state(0);
+  import { base } from "$app/paths";
+  import { Math } from "svelte-math";
 </script>
 
 <svelte:head>
   <title>Phobos</title>
 </svelte:head>
 
-<svelte:window onresize={handleWindowResize} />
-
-<section>
-  <div class="mg title">
-    <h1>Phobos</h1>
+<!-- First section, contains logo and a motto -->
+<section id="section-1" class="first">
+  <div class="mg title-card">
+    <div class="title">
+      <img src="{base}/favicon.svg" alt="Logo">
+      <h1>Phobos</h1>
+    </div>
+    
     <h2>Maths - Visualized</h2>
     <hr />
   </div>
-</section>
 
-<section>
-  <div class="mg">
-    <h2>Open Source</h2>
+  <div class="navigation-buttons">
+    <a class="button" href="#section-2">Learn More</a>
+    <a class="button" href="{base}/visualizations">Visualizations</a>
   </div>
 </section>
 
-<section>
+<!-- Second section, explains values -->
+<section id="section-2">
   <div class="mg">
-    <h2>Knowledge without a pricetag</h2>
+    <h2>Open Source Knowledge without a Pricetag</h2>
   </div>
 </section>
 
-<!-- Background -->
-<!-- TODO: make the UI look less weird -->
-<Graph
-  width={width}
-  height={height}
-  allowsUserInput={false}
-  id="background-graph"
-/>
+<!-- Third section, shows some demos -->
+<section id="section-3">
+  <div class="mg explanation">
+    <h2>Handcrafted Visualizations</h2>
+    <p>Interactive visualizations handcrafted to be as intuitive as possible to learn with.</p>
+  </div>
+
+  <div class="mg">
+    <h2>Check Some Out</h2>
+    <a class="button" href="{base}/visualizations">Click here</a>
+
+    <Graph
+      width={640}
+      height={360}
+      allowsUserInput={false}
+      hasNiceBorders={true}
+      id="visualizations-graph"
+    />
+  </div>
+</section>
 
 <style>
-  :global(#background-graph) {
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: -1;
-  }
-
   section {
-    min-height: 60vh;
+    min-height: 100vh;
 
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-  }
 
-  .title {
-    padding: 3em;
-    align-self: center;
-
-    h1 {
-      font-size: 5rem;
+    /* The first section will be shorter since the header will take up space and there is also some extra padding */
+    &.first {
+      /*
+        Base height - 100vh
+        Subtract 1em for padding of <main>
+        Subtract 1em for the top margin of the <header>
+        Subtract 75px for the height of the <header>
+        Subtract 2em for the padding of the <header>
+      */
+      min-height: calc(100vh - 1em - 1em - 75px - 2em);
     }
   }
 
-  /*functions={[
-    {
-      name: "background",
-      color: () => "red",//Styles.ContrastRed, TODO, doesn't work
-      func: Sin,
+  #section-1 {
+    gap: 5em;
+
+    .title-card {
+      padding: 3em;
+      align-self: center;
+
+      .title {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 1em;
+
+        h1 {
+          font-size: 5rem;
+        }
+
+        img {
+          height: 5em;
+        }
+      }
     }
-  ]}*/
+
+    .navigation-buttons {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      gap: 1em;
+
+      a {
+        width: 150px;
+        text-align: center;
+      }
+    }
+  }
+
+  #section-2 {
+  }
+
+  #section-3 {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 1em;
+
+    .mg {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 1em;
+
+      height: 525px;
+    }
+
+    .explanation {
+      max-width: 30vw;
+    }
+  }
 </style>
