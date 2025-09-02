c2b604106ebfe670cb5613467b39e50401b14b76
Destructor_Ben <62684035+Destructor-Ben@users.noreply.github.com>
2025-09-03 09:09:41 +1200

    Update lerp.ts

--- src/lib/maths/lerp.ts
+++ src/lib/maths/lerp.ts
@@ -1,8 +1,10 @@
+// Linearly blends between start and end based on t
 export function lerp(start: number, end: number, t: number): number {
     return start + (end - start) * t;
 }
 
-// Use on the t value of lerp, e.g. lerp(a, b, smoothstep(t))
+// Smooths the input t value
+// Use on the t value of lerp to smooth animations, e.g. lerp(a, b, smoothstep(t))
 export function smoothstep(t: number): number {
     return (t * t * (3 - 2 * t));
 }
